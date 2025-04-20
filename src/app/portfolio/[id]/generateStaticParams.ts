4bd9af93326bded1bc659486580a7652793f8464
steward379 <steward379@gmail.com>
2025-04-21 01:42:52 +0800

    fix

--- src/app/portfolio/[id]/generateStaticParams.ts
+++
@@ -1,6 +0,0 @@
-// src/app/portfolio/[id]/generateStaticParams.ts
-import { projects } from '@/data/projects';
-
-export async function generateStaticParams(): Promise<Array<{ id: string }>> {
-  return projects.map((project) => ({ id: project.id }));
-}
